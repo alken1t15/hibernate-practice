77a88c899a87058036aeaef32c3ea87c5230180d
Alexey Zarubin <alken1t15@gmail.com>
2023-02-02 22:28:58 +0600

    first commit

---
+++ src/main/java/alken1t/catalog/JpaLesson.java
@@ -0,0 +1,4 @@
+package alken1t.catalog;
+public class JpaLesson {
+
+}
---
+++ src/main/java/alken1t/catalog/entity/Category.java
@@ -0,0 +1,14 @@
+package alken1t.catalog.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
